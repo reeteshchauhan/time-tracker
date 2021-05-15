7ad98e861581882c41e2e5036ec8ae91dcf55601
reeteshchauhan <73590581+reeteshchauhan@users.noreply.github.com>
2021-05-15 17:42:25 +0530

    Made changes for testing

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -1,7 +1,7 @@
 package training.taylor.timetracker.core;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
 import training.taylor.timetracker.core.dao.TimeEntry;
 
 import java.util.List;
@@ -26,7 +26,7 @@ public class Tracker {
         return entries.size();
     }
 
-    public TimeEntry get(int index) {
+   // public TimeEntry get(int index) {
         try {
 
         } catch (Exception e) {
